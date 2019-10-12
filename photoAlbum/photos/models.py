2d489f75685f77d80feee63e41f7694bf3d25710
Britney <britney.gibbs@student.montana.edu>
2019-10-11 18:11:24 -0600

    Structured models better

--- photoAlbum/photos/models.py
+++ photoAlbum/photos/models.py
@@ -6,88 +6,41 @@ class Photo(models.Model):
     document = models.ImageField()
     date = models.DateField(("Date"), auto_now_add=True)
 
-"""
-class AnimalMB(models.Model):
-    photo = models.ForeignKey(Photo, on_delete-models.PROTECT)
-
-
-class AnimalSampson(models.Model):
-    photo = models.ForeignKey(Photo, on_delete-models.PROTECT)
-
-
-class AnimalKitty(models.Model):
-    photo = models.ForeignKey(Photo, on_delete-models.PROTECT)
-
-
-class AnimalPumpkin(models.Model):
-    photo = models.ForeignKey(Photo, on_delete-models.PROTECT)
+    def __str__(self):
+        return self.photo_hash
 
 
 class Animal(models.Model):
-    photo = models.ForeignKey(Photo, on_delete-models.PROTECT)
+    name = models.CharField(max_length=500)
 
-class Animal(models.Model):
-    photo = models.ForeignKey(Photo, on_delete=models.PROTECT)
-    mb = models.IntegerField(default=0)
-    sampson = models.IntegerField(default=0)
-    kitty = models.IntegerField(default=0)
-    pumpkin = models.IntegerField(default=0)
-    gisela = models.IntegerField(default=0)
-    other = models.CharField(max_length=500, default='')
-
-"""
-
-class Animal(models.Model):
-    photo = models.ForeignKey(Photo, on_delete=models.PROTECT)
-
-    class Meta:
-        abstract = True
-
-
-class Sampson(Animal):
-    pass
-
-class Korra(Animal):
-    pass
-
-
-class MB(Animal):
-    pass
-
-
-class Pumpkin(Animal):
-    pass
-
+    def __str__(self):
+        return self.name
 
 
 class Person(models.Model):
-    photo = models.ForeignKey(Photo, on_delete=models.PROTECT)
+    name = models.CharField(max_length=500)
 
-    class Meta:
-        abstract = True
-
-class Peggy(Person):
-    pass
-
-class Carman(Person):
-    pass
+    def __str__(self):
+        return self.name
 
 
-"""
-class PersonOld(models.Model):
-    photo = models.ForeignKey(Photo, on_delete=models.PROTECT)
-    peggy = models.IntegerField(default=0)
-    britney = models.IntegerField(default=0)
-    barbara = models.IntegerField(default=0)
-    carman = models.IntegerField(default=0)
-    ivan = models.IntegerField(default=0)
-    erika = models.IntegerField(default=0)
-    rachel = models.IntegerField(default=0)
+class Location(models.Model):
+    name = models.CharField(max_length=500)
+
+    def __str__(self):
+        return self.name
 
 
-class Places(models.Model):
-    photo = models.ForeignKey(Photo, on_delete=models.PROTECT)
-    bozeman = models.IntegerField(default=0)
-    great_falls = models.IntegerField(default=0)
+class Event(models.Model):
+    name = models.CharField(max_length=500)
 
-"""
+    def __str__(self):
+        return self.name
+
+
+# use for other things that aren't included (ex: memes)
+class Classifier(models.Model):
+    name = models.CharField(max_length=500)
+
+    def __str__(self):
+        return self.name
