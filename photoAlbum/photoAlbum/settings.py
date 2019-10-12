bd59876dbf296490e257582754112fb8950513d5
Britney <britney.gibbs@student.montana.edu>
2019-10-11 19:30:29 -0600

    Added media root

--- photoAlbum/photoAlbum/settings.py
+++ photoAlbum/photoAlbum/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/dev/ref/settings/
 """
 
 import os
+from decouple import config
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -119,3 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/dev/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Path for storing files
+MEDIA_ROOT = config('MEDIA_ROOT')
+MEDIA_URL = '/files/'
