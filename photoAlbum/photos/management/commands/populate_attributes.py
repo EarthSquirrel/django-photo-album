21edb7fe75885d7943300f1cd004466ab7403257
EarthSquirrel <britneyandcaddy1@gmail.com>
2019-12-16 10:30:24 -0700

    Changed json keys to plural and lower case

--- photoAlbum/photos/management/commands/populate_attributes.py
+++ photoAlbum/photos/management/commands/populate_attributes.py
@@ -12,28 +12,28 @@ class Command(BaseCommand):
             attribs = json.load(json_file)
             print('Creating People!')
             try:
-                for p in attribs['Person']:
+                for p in attribs['people']:
                     models.Person.objects.create(name=p)
                     print('Created person: {}'.format(p))
             except KeyError:
                 print('attributes.json key error. Missing Person')
             print('\nCreating Animals')
             try:
-                for p in attribs['Animal']:
+                for p in attribs['animals']:
                     models.Animal.objects.create(name=p)
                     print('Created animal: {}'.format(p))
             except KeyError:
                 print('attributes.json key error. Missing Animal')
             print('\nCreating Events!')
             try:
-                for p in attribs['Event']:
+                for p in attribs['events']:
                     models.Event.objects.create(name=p)
                     print('Created event: {}'.format(p))
             except KeyError:
                 print('attributes.json key error. Missing Event')
             print('\nCreating Classifiers!')
             try:
-                for p in attribs['Classifier']:
+                for p in attribs['classifiers']:
                     models.Classifier.objects.create(name=p)
                     print('Created classifier: {}'.format(p))
             except KeyError:
