12656f2052b4e9881dd9491574b662b758482772
EarthSquirrel <britneyandcaddy1@gmail.com>
2019-12-27 14:59:24 -0700

    Changed create to get_or_create

--- photoAlbum/photos/management/commands/populate_attributes.py
+++ photoAlbum/photos/management/commands/populate_attributes.py
@@ -13,28 +13,28 @@ class Command(BaseCommand):
             print('Creating People!')
             try:
                 for p in attribs['people']:
-                    models.Person.objects.create(name=p)
+                    models.Person.objects.get_or_create(name=p)
                     print('Created person: {}'.format(p))
             except KeyError:
                 print('attributes.json key error. Missing Person')
             print('\nCreating Animals')
             try:
                 for p in attribs['animals']:
-                    models.Animal.objects.create(name=p)
+                    models.Animal.objects.get_or_create(name=p)
                     print('Created animal: {}'.format(p))
             except KeyError:
                 print('attributes.json key error. Missing Animal')
             print('\nCreating Events!')
             try:
                 for p in attribs['events']:
-                    models.Event.objects.create(name=p)
+                    models.Event.objects.get_or_create(name=p)
                     print('Created event: {}'.format(p))
             except KeyError:
                 print('attributes.json key error. Missing Event')
             print('\nCreating Classifiers!')
             try:
                 for p in attribs['classifiers']:
-                    models.Classifier.objects.create(name=p)
+                    models.Classifier.objects.get_or_create(name=p)
                     print('Created classifier: {}'.format(p))
             except KeyError:
                 print('attributes.json key error. Missing Classifier')
